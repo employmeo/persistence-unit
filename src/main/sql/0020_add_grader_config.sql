c6c19b66fbcec5383991699281fa8e767b597b1d
Sri <sridharkaza@yahoo.com>
2016-12-11 16:44:26 -0800

    fix to feature - grader config

--- src/main/sql/0020_add_grader_config.sql
+++ src/main/sql/0020_add_grader_config.sql
@@ -11,7 +11,7 @@ CREATE TABLE employmeo.grader_configs
   CONSTRAINT grader_config_grader_config_asid_fkey FOREIGN KEY (grader_config_asid)
       REFERENCES employmeo.account_surveys (as_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT grader_config_grader_user_id_fkey FOREIGN KEY (grader_user_id)
+  CONSTRAINT grader_config_grader_user_id_fkey FOREIGN KEY (grader_config_user_id)
       REFERENCES employmeo.users (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
@@ -20,16 +20,16 @@ WITH (
 );
 
 
-ALTER TABLE employmeo.grader ADD grader_notes text;
-ALTER TABLE employmeo.grader ADD grader_modified_date timestamp with time zone;
+ALTER TABLE employmeo.graders ADD grader_notes text;
+ALTER TABLE employmeo.graders ADD grader_modified_date timestamp with time zone;
 
 ALTER TABLE employmeo.responses ALTER COLUMN response_text TYPE text;
 ALTER TABLE employmeo.grades ALTER COLUMN grade_text TYPE text;
 
 --//@UNDO
 
-ALTER TABLE employmeo.grader DROP grader_notes;
-ALTER TABLE employmeo.grader DROP grader_modified_date;
+ALTER TABLE employmeo.graders DROP grader_notes;
+ALTER TABLE employmeo.graders DROP grader_modified_date;
 
 ALTER TABLE employmeo.responses ALTER COLUMN response_text TYPE character varying(255);
 ALTER TABLE employmeo.grades ALTER COLUMN grade_text TYPE character varying(255);
