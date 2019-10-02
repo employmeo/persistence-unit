f4e261dcb8cb15df1653fbb140df2a2a2a24b729
Sri <sridharkaza@yahoo.com>
2019-10-02 12:22:36 -0700

    more code for reference reminders, etc.

--- src/main/java/com/employmeo/data/repository/UserRepository.java
+++ src/main/java/com/employmeo/data/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.employmeo.data.repository;
 
+import java.util.Date;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -26,5 +28,9 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
 	@Query
 	Set<User> findAllByUserAccountId(Long userAccountId);
+	
+	@Modifying
+	@Query("update User user set user.lastLogin = ?1 where user.id = ?2")
+	public void setLastLogin(Date date, Long id);
 		
 }
